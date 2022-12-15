fe72982d766ff51e5861ed9058e69dfd526fdda4
Kevin Adam Prasetya <72896188+kkms51@users.noreply.github.com>
2022-12-15 01:51:49 -0800

    Add files via upload

---
+++ Days 1/jawaban/helloworld.py
@@ -0,0 +1,41 @@
+#LVL1&2
+print(5+6)
+print(5-6)
+print(5*6)
+print(5%6)
+print(5/6)
+print(5**6)
+print(5//6)
+
+print("")
+
+print("Kevin Adam Prasetya")
+print("105220010")
+print("Jakarta")
+
+print("")
+
+print(type(12))
+print(type(7.4))
+print(type(3.14))
+print(type(5-6j))
+print(type(['Budi','105201001', 'Jakarta']))
+print(type("Kevin Adam Prasetya"))
+print(type("105220010"))
+print(type("Jakarta"))
+
+print("")
+
+#LVL 3
+print(type(50))
+print(type(12.6))
+print(type("12"))
+print(type(True))
+print(type(['Kevin','105220010', 'Jakarta']))
+print(type(("Budi","105201001", "Jakarta")))
+print(type({"Budi","105201001", "Jakarta"}))
+print(type({"Nama":"Kevin"}))
+
+print("")
+
+print(((10-2)**2)+((8-3)**2))
