050af0b2fe6dea53b29bdc3f614024082987a263
Kevin Adam Prasetya <72896188+kkms51@users.noreply.github.com>
2022-12-15 02:21:26 -0800

    Add files via upload

---
+++ Days 2/jawaban/Day2.py
@@ -0,0 +1,101 @@
+#Hari ke-2: Fundamental Python
+
+
+#LVL 1
+
+nama = "Kevin Adam Prasetya"
+
+jurusan = "Ilmu Komputer"
+
+angkatan = 2020
+
+umur = 20
+
+asal_daerah = "Jakarta"
+
+money_amount = 100000
+
+is_mahasiswa = True
+
+is_alien = False
+
+
+#LVL2
+print(type(nama))
+
+print(type(jurusan))
+
+print(type(angkatan))
+print(type(umur))
+
+print(type(asal_daerah))
+print(type(money_amount))
+
+print(type(is_mahasiswa))
+print(type(is_alien))
+
+print("")
+
+print(len(nama))
+
+print("")
+
+
+num_one=5
+
+num_two=4
+
+
+total=num_one+num_two
+print(total)
+
+diff=num_two-num_one
+
+print(diff)
+
+product=num_two*num_one
+print(product)
+
+division=num_one/num_two
+
+print(division)
+
+remainder=num_two%num_one
+print(remainder)
+
+exp=num_one**num_two
+
+print(exp)
+
+floor_division=num_one//num_two
+
+print(floor_division)
+
+print("")
+
+
+input("Nama: ")
+
+input("Umur: ")
+
+input("Asal Daerah: ")
+
+print("")
+
+
+area_of_circle=3.14*30*30
+
+print(area_of_circle)
+
+circum_of_circle=2*3.14*30
+
+print(circum_of_circle)
+
+x = float(input("Radius: "))
+luas = 3.14*x*x
+print(luas)
+
+print("")
+
+
+help('keywords')
